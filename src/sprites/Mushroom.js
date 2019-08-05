5eb4710c53565baa364dcb50e9a37ebac3b454ba
glagolev <glagolev@astralnalog.ru>
2019-08-05 16:16:19 +0300

    Начинаем изучать фазер

--- src/sprites/Mushroom.js
+++ src/sprites/Mushroom.js
@@ -1,7 +1,19 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
 export default class extends Phaser.GameObjects.Sprite {
-  constructor ({ scene, x, y, asset }) {
-    super(scene, x, y, asset)
+  constructor({ scene, x, y, asset }) {
+    super(scene, x, y, asset);
+
+    this.angleDelta = 1;
+    this.setInteractive();
+    this.on("pointerdown", this.onClick, this);
+  }
+
+  onClick() {
+    this.angleDelta = -this.angleDelta;
+  }
+
+  update() {
+    this.angle += this.angleDelta;
   }
 }
