bb2f9269595fc55ec359dd26ede90412e682bead
Глагольев Игорь <defazze@gmail.com>
2019-08-12 21:35:30 +0300

    Inventory drag'n'drop

--- src/scenes/Inventory.js
+++ src/scenes/Inventory.js
@@ -22,32 +22,27 @@ export default class extends Phaser.Scene {
       .setInteractive();
     battlefield.on("pointerdown", () => this.scene.switch("GameScene"));
 
-    //this.add.sprite(528, 432, "inventory");
+    const backgroundWidth = COLUMNS * CELL_SIZE + (COLUMNS + 1) * BORDER_WIDTH;
+    const backgroundHeight = ROWS * CELL_SIZE + (ROWS + 1) * BORDER_WIDTH;
 
-    const background = this.add.graphics();
-    background.fillStyle(0xffffff, 1);
-    background.fillRect(
-      X,
-      Y,
-      COLUMNS * CELL_SIZE + (COLUMNS + 1) * BORDER_WIDTH,
-      ROWS * CELL_SIZE + (ROWS + 1) * BORDER_WIDTH
+    const background = this.add.rectangle(
+      X + backgroundWidth / 2,
+      Y + backgroundHeight / 2,
+      backgroundWidth,
+      backgroundHeight,
+      0xffffff
     );
 
     for (var i = 0; i < COLUMNS; i++) {
       for (var j = 0; j < ROWS; j++) {
-        const x = X + CELL_SIZE * i + BORDER_WIDTH * (i + 1);
-        const y = Y + CELL_SIZE * j + BORDER_WIDTH * (j + 1);
+        const x = X + CELL_SIZE * (i + 0.5) + BORDER_WIDTH * (i + 1);
+        const y = Y + CELL_SIZE * (j + 0.5) + BORDER_WIDTH * (j + 1);
 
-        const cell = this.add.graphics();
+        const cell = this.add.rectangle(x, y, CELL_SIZE, CELL_SIZE, 0x838383);
+        cell.setInteractive(undefined, undefined, true);
+
         cell.columnIndex = i;
         cell.rowIndex = j;
-        cell.fillStyle(0x838383, 1);
-        cell.fillRect(x, y, CELL_SIZE, CELL_SIZE);
-        cell.setInteractive(
-          new Phaser.Geom.Rectangle(x, y, CELL_SIZE, CELL_SIZE),
-          Phaser.Geom.Rectangle.Contains,
-          true
-        );
       }
     }
 
@@ -67,28 +62,25 @@ export default class extends Phaser.Scene {
     });
 
     this.input.on("dragenter", function(pointer, gameObject, dropZone) {
-      console.log("move");
-      dropZone.fillStyle(0x777676, 1);
+      dropZone.setFillStyle(0x777676, 1);
     });
 
     this.input.on("dragleave", function(pointer, gameObject, dropZone) {
-      //dropZone.fillStyle(0x838383, 1);
+      dropZone.setFillStyle(0x838383, 1);
     });
 
     this.input.on("drop", function(pointer, gameObject, dropZone) {
-      gameObject.x =
-        X +
-        dropZone.columnIndex * CELL_SIZE +
-        (dropZone.columnIndex + 1) * BORDER_WIDTH +
-        CELL_SIZE / 2;
-      gameObject.y =
-        Y +
-        dropZone.rowIndex * CELL_SIZE +
-        (dropZone.rowIndex + 1) * BORDER_WIDTH +
-        CELL_SIZE / 2;
+      gameObject.x = dropZone.x;
+      gameObject.y = dropZone.y;
 
-      console.log(dropZone);
-      //zone.clearTint();
+      dropZone.setFillStyle(0x838383, 1);
+    });
+
+    this.input.on("dragend", function(pointer, gameObject, dropped) {
+      if (!dropped) {
+        gameObject.x = gameObject.input.dragStartX;
+        gameObject.y = gameObject.input.dragStartY;
+      }
     });
   }
 }
