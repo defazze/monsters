f492070054f79f650b68c9488413e0d3edec4263
Глагольев Игорь <defazze@gmail.com>
2019-08-12 20:34:58 +0300

    Inventory move

--- src/scenes/Inventory.js
+++ src/scenes/Inventory.js
@@ -1,5 +1,12 @@
 import Phaser from "phaser";
 
+const CELL_SIZE = 48;
+const BORDER_WIDTH = 4;
+const COLUMNS = 12;
+const ROWS = 6;
+const X = 200;
+const Y = 400;
+
 export default class extends Phaser.Scene {
   constructor() {
     super({ key: "InventoryScene" });
@@ -15,6 +22,73 @@ export default class extends Phaser.Scene {
       .setInteractive();
     battlefield.on("pointerdown", () => this.scene.switch("GameScene"));
 
-    this.add.sprite(528, 432, "inventory");
+    //this.add.sprite(528, 432, "inventory");
+
+    const background = this.add.graphics();
+    background.fillStyle(0xffffff, 1);
+    background.fillRect(
+      X,
+      Y,
+      COLUMNS * CELL_SIZE + (COLUMNS + 1) * BORDER_WIDTH,
+      ROWS * CELL_SIZE + (ROWS + 1) * BORDER_WIDTH
+    );
+
+    for (var i = 0; i < COLUMNS; i++) {
+      for (var j = 0; j < ROWS; j++) {
+        const x = X + CELL_SIZE * i + BORDER_WIDTH * (i + 1);
+        const y = Y + CELL_SIZE * j + BORDER_WIDTH * (j + 1);
+
+        const cell = this.add.graphics();
+        cell.columnIndex = i;
+        cell.rowIndex = j;
+        cell.fillStyle(0x838383, 1);
+        cell.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+        cell.setInteractive(
+          new Phaser.Geom.Rectangle(x, y, CELL_SIZE, CELL_SIZE),
+          Phaser.Geom.Rectangle.Contains,
+          true
+        );
+      }
+    }
+
+    const potion = this.add
+      .image(
+        X + BORDER_WIDTH + CELL_SIZE / 2,
+        Y + BORDER_WIDTH + CELL_SIZE / 2,
+        "hp-potion"
+      )
+      .setInteractive();
+
+    this.input.setDraggable(potion);
+
+    this.input.on("drag", function(pointer, gameObject, dragX, dragY) {
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+    });
+
+    this.input.on("dragenter", function(pointer, gameObject, dropZone) {
+      console.log("move");
+      dropZone.fillStyle(0x777676, 1);
+    });
+
+    this.input.on("dragleave", function(pointer, gameObject, dropZone) {
+      //dropZone.fillStyle(0x838383, 1);
+    });
+
+    this.input.on("drop", function(pointer, gameObject, dropZone) {
+      gameObject.x =
+        X +
+        dropZone.columnIndex * CELL_SIZE +
+        (dropZone.columnIndex + 1) * BORDER_WIDTH +
+        CELL_SIZE / 2;
+      gameObject.y =
+        Y +
+        dropZone.rowIndex * CELL_SIZE +
+        (dropZone.rowIndex + 1) * BORDER_WIDTH +
+        CELL_SIZE / 2;
+
+      console.log(dropZone);
+      //zone.clearTint();
+    });
   }
 }
--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -17,11 +17,11 @@ export default class extends Phaser.Scene {
     this.load.image("arrow", "assets/images/arrow.png");
     this.load.image("chest", "assets/images/chest.png");
     this.load.image("hero", "assets/images/hero.png");
-    this.load.image("hp-potion", "assets/images/hp-potion.png");
+    this.load.image("hp-potion", "assets/images/hp-potion2.png");
     this.load.image("castle", "assets/images/castle.png");
     this.load.image("castle-big", "assets/images/castle-big.png");
     this.load.image("battlefield", "assets/images/battlefield.png");
-    this.load.image("inventory", "assets/images/inventory.jpg");
+    this.load.image("inventory", "assets/images/inventory.png");
     this.load.image("inventory-icon", "assets/images/inventory-icon.png");
     this.load.spritesheet("coin", "assets/images/coin.png", {
       frameWidth: 16,
